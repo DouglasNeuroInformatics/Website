de180e75e65a341ddaaf8d04246d9eb7fb66e794
joshunrau <contact@joshuaunrau.com>
2023-09-01 12:30:56 -0400

    add config

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,43 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig, sharpImageService } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import getReadingTime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  build: {
+    assets: '_assets'
+  },
+  compressHTML: true,
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService()
+  },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          fr: 'fr'
+        }
+      }
+    }),
+    tailwind()
+  ],
+  markdown: {
+    remarkPlugins: [
+      () => {
+        return function (tree, { data }) {
+          const textOnPage = toString(tree);
+          const readingTime = getReadingTime(textOnPage);
+          data.astro.frontmatter.readingTime = readingTime.minutes;
+        };
+      }
+    ]
+  },
+  site: 'https://douglasneuroinformatics.ca'
 });
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import typographyPlugin from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem'
+        }
+      },
+      transitionProperty: {
+        mh: 'max-height',
+        'opacity-transform': 'opacity, transform'
+      }
+    }
+  },
+  plugins: [typographyPlugin]
+};
