9a397c894dd98d3beb961b5d1fe68c03084bc4e2
david roper <davidroper321@gmail.com>
2023-09-15 11:42:34 -0400

    testimonial edit

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -20,17 +20,15 @@ const TestimonialCard = ({
       )}
       onClick={onClick}
     >
-      <div>
-        <div className="flex items-center justify-center">
-          <img alt={fullName} className="rounded-lg object-cover shadow-lg" src={image.src} />
-          <div className="col-span-2 ml-auto space-y-1 text-lg font-medium leading-relaxed md:ml-4 md:mr-auto">
-            <h3>{`${fullName}${suffix ? `, ${suffix}` : ''}`}</h3>
-            <span className="text-sky-700 dark:text-sky-400">{position}</span>
-            <p className="mt-1 line-clamp-3 text-base font-normal leading-tight text-slate-600 dark:text-slate-300">
-              {quote}
-            </p>
-          </div>
-        </div>
+      <div className="flex items-center justify-center">
+        <img alt={fullName} className="rounded-lg object-cover shadow-lg" src={image.src} />
+      </div>
+      <div className="col-span-2 space-y-1 text-lg font-medium leading-relaxed">
+        <h3>{`${fullName}${suffix ? `, ${suffix}` : ''}`}</h3>
+        <span className="text-sky-700 dark:text-sky-400">{position}</span>
+        <p className="mt-1 line-clamp-3 text-base font-normal leading-tight text-slate-600 dark:text-slate-300">
+          {quote}
+        </p>
       </div>
     </button>
   );
