9898db40d85bf6ccb6c55f7c461bede1ce51f9b2
david roper <davidroper321@gmail.com>
2023-09-15 11:35:03 -0400

    edits to testimonial format

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -23,13 +23,13 @@ const TestimonialCard = ({
       <div>
         <div className="flex items-center justify-center">
           <img alt={fullName} className="rounded-lg object-cover shadow-lg" src={image.src} />
-        </div>
-        <div className="col-span-2 space-y-1 text-lg font-medium leading-relaxed">
-          <h3>{`${fullName}${suffix ? `, ${suffix}` : ''}`}</h3>
-          <span className="text-sky-700 dark:text-sky-400">{position}</span>
-          <p className="mt-1 line-clamp-3 text-base font-normal leading-tight text-slate-600 dark:text-slate-300">
-            {quote}
-          </p>
+          <div className="col-span-2 ml-auto space-y-1 text-lg font-medium leading-relaxed md:ml-4 md:mr-auto">
+            <h3>{`${fullName}${suffix ? `, ${suffix}` : ''}`}</h3>
+            <span className="text-sky-700 dark:text-sky-400">{position}</span>
+            <p className="mt-1 line-clamp-3 text-base font-normal leading-tight text-slate-600 dark:text-slate-300">
+              {quote}
+            </p>
+          </div>
         </div>
       </div>
     </button>
