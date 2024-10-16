47bfb7e3fe1dabcbf27cce6e3e0d4f230235712e
joshunrau <contact@joshuaunrau.com>
2024-10-16 13:06:44 -0400

    setup config

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,33 @@
 import path from 'node:path';
 
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  build: {
+    assets: '_assets'
+  },
+  compressHTML: true,
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          fr: 'fr'
+        }
+      }
+    }),
+    tailwind({
+      configFile: path.resolve(import.meta.dirname, 'tailwind.config.cjs')
+    })
+  ],
+  output: 'static',
+  server: {
+    port: 3000
+  },
+  site: 'https://douglasneuroinformatics.ca',
   vite: {
     resolve: {
       alias: {
