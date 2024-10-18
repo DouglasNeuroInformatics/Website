b62442ed61ab1f31e6fa613e5a035b9578c2a592
joshunrau <contact@joshuaunrau.com>
2024-10-18 15:54:31 -0400

    setup theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const typographyPlugin = require('@tailwindcss/typography');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
+  darkMode: ['class', '[data-mode="dark"]'],
   plugins: [typographyPlugin()],
   root: __dirname,
   theme: {
