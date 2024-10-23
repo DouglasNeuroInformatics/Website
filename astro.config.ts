ed3bd2c6d70e7be30e276fe30878bcd4e5e5a464
joshunrau <contact@joshuaunrau.com>
2024-10-23 17:35:57 -0400

    remove react

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import path from 'node:path';
 
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -23,7 +22,7 @@ export default defineConfig({
     }
   }), tailwind({
     configFile: path.resolve(import.meta.dirname, 'tailwind.config.cjs')
-  }), react()],
+  })],
   markdown: {
     remarkPlugins: [
       () => {
