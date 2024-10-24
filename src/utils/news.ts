58ca61f9f4fd2332e7b1a747987380e6ec54d3f9
joshunrau <contact@joshuaunrau.com>
2024-10-24 00:59:01 -0400

    fix regex

--- src/utils/news.ts
+++ src/utils/news.ts
@@ -1,7 +1,7 @@
 import type { Language } from '@/i18n';
 
 export function parsePostSlug(slug: string) {
-  const regex = /(en|fr)\/(\d{4}-\d{2}-\d{2})_([^=])+/;
+  const regex = /(en|fr)\/(\d{4}-\d{2}-\d{2})_([^=]+)/;
   const result = regex.exec(slug);
   if (!result) {
     throw new Error(`Unexpected format for slug '${slug}': must match regular expression '${regex.source}'`);
