2ec66b42c606694703fc3876016b67a97d437988
joshunrau <contact@joshuaunrau.com>
2024-10-24 00:54:49 -0400

    add news util

---
+++ src/utils/news.ts
@@ -0,0 +1,19 @@
+import type { Language } from '@/i18n';
+
+export function parsePostSlug(slug: string) {
+  const regex = /(en|fr)\/(\d{4}-\d{2}-\d{2})_([^=])+/;
+  const result = regex.exec(slug);
+  if (!result) {
+    throw new Error(`Unexpected format for slug '${slug}': must match regular expression '${regex.source}'`);
+  }
+  const [locale, dateString, name] = result.slice(1) as [Language, string, string];
+  const timestamp = Date.parse(dateString);
+  if (Number.isNaN(timestamp)) {
+    throw new Error(`Invalid date '${dateString}' in slug '${slug}'`);
+  }
+
+  return {
+    datePublished: new Date(timestamp),
+    url: `/${locale}/news/${name}`
+  };
+}
