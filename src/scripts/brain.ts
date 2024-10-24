7d4807534f45762899b7fbe83ab8adeca1d9ab3c
joshunrau <contact@joshuaunrau.com>
2024-10-24 14:55:22 -0400

    add brain

---
+++ src/scripts/brain.ts
@@ -0,0 +1,24 @@
+import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+
+const container = document.getElementById('brain-container')!;
+
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
+
+const renderer = new WebGLRenderer();
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+renderer.setSize(500, 500);
+container.appendChild(renderer.domElement);
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
