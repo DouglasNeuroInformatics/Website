d9519ca292a29eaf32d27d20781bd90dc674c9ee
joshunrau <contact@joshuaunrau.com>
2024-11-13 21:56:34 -0500

    parse post

--- src/utils/news.ts
+++ src/utils/news.ts
@@ -3,16 +3,17 @@ import type { CollectionEntry } from 'astro:content';
 
 import type { Language } from '@/i18n';
 
-function parsePostSlug(slug: string) {
+function parsePost(post: CollectionEntry<'news'>) {
   const regex = /(en|fr)\/(\d{4}-\d{2}-\d{2})_([^=]+)/;
-  const result = regex.exec(slug);
+  const result = regex.exec(post.slug);
   if (!result) {
-    throw new Error(`Unexpected format for slug '${slug}': must match regular expression '${regex.source}'`);
+    throw new Error(`Unexpected format for slug '${post.slug}': must match regular expression '${regex.source}'`);
   }
+
   const [language, dateString, name] = result.slice(1) as [Language, string, string];
   const timestamp = Date.parse(dateString);
   if (Number.isNaN(timestamp)) {
-    throw new Error(`Invalid date '${dateString}' in slug '${slug}'`);
+    throw new Error(`Invalid date '${dateString}' in slug '${post.slug}'`);
   }
 
   const datePublished = new Date(timestamp);
@@ -21,7 +22,7 @@ function parsePostSlug(slug: string) {
     datePublished,
     language,
     name,
-    url: `/${language}/news/${toBasicISOString(datePublished)}/${name}`
+    url: post.data.type === 'link' ? post.data.url : `/${language}/news/${toBasicISOString(datePublished)}/${name}`
   };
 }
 
@@ -42,4 +43,4 @@ function formatAuthors(authors: CollectionEntry<'team'>[], language: Language) {
     .join(' ');
 }
 
-export { formatAuthors, parsePostSlug };
+export { formatAuthors, parsePost };
