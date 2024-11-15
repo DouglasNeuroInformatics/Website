6700b76e953a47359461b69e7189deda6d6c729c
joshunrau <contact@joshuaunrau.com>
2024-11-15 15:53:28 -0500

    make project link nullable

--- src/content/config.ts
+++ src/content/config.ts
@@ -34,7 +34,7 @@ export const collections = {
         en: z.string().min(1),
         fr: z.string().min(1)
       }),
-      link: z.string().url(),
+      link: z.string().url().nullable(),
       technologies: z.array(reference('technologies')).min(1),
       title: z.object({
         en: z.string().min(1),
