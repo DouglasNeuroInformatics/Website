8a2d0b016f6d4724d458f97a943dbdc253fc7382
joshunrau <contact@joshuaunrau.com>
2024-11-15 13:54:28 -0500

    update formatting

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,20 +1,23 @@
 import type { CollectionEntry } from 'astro:content';
 
-import type { Language } from '@/i18n';
-
-export function formatTeamMembers(teamMembers: CollectionEntry<'team'>[], language: Language) {
+export function formatTeamMembers(teamMembers: CollectionEntry<'team'>[]) {
   if (teamMembers.length === 1) {
     return `${teamMembers[0]!.data.fullName}${teamMembers[0]!.data.suffix ? `, ${teamMembers[0]!.data.suffix}` : ''}`;
   }
   return teamMembers
-    .map((author, i) => {
+    .map((teamMember, i) => {
+      if (i === 3) {
+        return ', et al.';
+      } else if (i > 3) {
+        return;
+      }
       let separator: string | undefined;
       if (i === teamMembers.length - 1) {
-        separator = { en: ' and ', fr: ' et ' }[language];
+        separator = ' & ';
       } else if (i > 0) {
         separator = ', ';
       }
-      return separator ? separator + author.data.fullName : author.data.fullName;
+      return separator ? separator + teamMember.data.fullName : teamMember.data.fullName;
     })
     .join('');
 }
