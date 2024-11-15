2989df18c616f4390512be661c8e4e7089abc184
joshunrau <contact@joshuaunrau.com>
2024-11-15 12:51:48 -0500

    rename formatAuthors

---
+++ src/utils/format.ts
@@ -0,0 +1,20 @@
+import type { CollectionEntry } from 'astro:content';
+
+import type { Language } from '@/i18n';
+
+export function formatTeamMembers(teamMembers: CollectionEntry<'team'>[], language: Language) {
+  if (teamMembers.length === 1) {
+    return `${teamMembers[0]!.data.fullName}${teamMembers[0]!.data.suffix ? `, ${teamMembers[0]!.data.suffix}` : ''}`;
+  }
+  return teamMembers
+    .map((author, i) => {
+      let separator: string | undefined;
+      if (i === teamMembers.length - 1) {
+        separator = { en: 'and', fr: 'et' }[language];
+      } else if (i > 0) {
+        separator = ',';
+      }
+      return separator ? `${separator} ${author.data.fullName}` : author.data.fullName;
+    })
+    .join(' ');
+}
